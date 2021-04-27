8d337077ae1ebd9b61025d7197f2ea8131b35e47
Lien Pham <pham.van.lien@sun-asterisk.com>
2021-04-27 11:20:52 +0700

    task35757

--- app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
+++ app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.sun.excovid19
+package com.sun.excovid19.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sun.excovid19.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.SplashTheme)
         setContentView(R.layout.activity_main)
     }
 }
