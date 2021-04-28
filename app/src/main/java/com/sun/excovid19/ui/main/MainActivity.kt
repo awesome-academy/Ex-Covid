18aac6f8af51c073df434550ffdb09515cd889ef
Lien Pham <pham.van.lien@sun-asterisk.com>
2021-04-28 11:44:11 +0700

    task35759

--- app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
+++ app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
@@ -2,12 +2,19 @@ package com.sun.excovid19.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.sun.excovid19.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.SplashTheme)
+        setTheme(R.style.Covid19Theme)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentMainNavHost) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
