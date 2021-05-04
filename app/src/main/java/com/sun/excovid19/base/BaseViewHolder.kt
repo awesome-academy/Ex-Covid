3ab2d2f001708b3c47e61188cf0ff0b2be94b344
Lien Pham <pham.van.lien@sun-asterisk.com>
2021-05-04 10:18:58 +0700

    task35760CreateBaseClass

---
+++ app/src/main/java/com/sun/excovid19/base/BaseViewHolder.kt
@@ -0,0 +1,24 @@
+package com.sun.excovid19.base
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder<T>(
+    binding: ViewDataBinding,
+    click: (T) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private var itemData: T? = null
+
+    init {
+        binding.root.setOnClickListener {
+            itemData?.let {
+                click(it)
+            }
+        }
+    }
+
+    open fun bindData(item: T) {
+        itemData = item
+    }
+}
---
+++ app/src/main/java/com/sun/excovid19/base/UpdateDataAdapter.kt
@@ -0,0 +1,5 @@
+package com.sun.excovid19.base
+
+interface UpdateDataAdapter<T> {
+    fun setData(data: List<T>?)
+}
