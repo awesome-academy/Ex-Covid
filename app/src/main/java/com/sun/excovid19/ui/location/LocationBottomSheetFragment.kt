fe1bcc3983fbcf46412d0c4a80666e8f0e960bb0
Lien Pham <pham.van.lien@sun-asterisk.com>
2021-05-14 09:32:49 +0700

    task35771Internet

--- app/src/main/java/com/sun/excovid19/ui/location/LocationBottomSheetFragment.kt
+++ app/src/main/java/com/sun/excovid19/ui/location/LocationBottomSheetFragment.kt
@@ -4,18 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sun.excovid19.R
-import com.sun.excovid19.base.BaseFragment
 import com.sun.excovid19.databinding.BottomSheetWeekBinding
-import com.sun.excovid19.databinding.FragmentLocationBinding
-import com.sun.excovid19.ui.home.HomeViewModel
-import com.sun.excovid19.utils.showToast
-import kotlinx.android.synthetic.main.bottom_sheet_week.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LocationBottomSheetFragment : BottomSheetDialogFragment() {
 
@@ -38,5 +31,4 @@ class LocationBottomSheetFragment : BottomSheetDialogFragment() {
             day = args.day
         }
     }
-
 }
--- app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
+++ app/src/main/java/com/sun/excovid19/ui/main/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.sun.excovid19.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.sun.excovid19.R
+import com.sun.excovid19.utils.NetworkUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var isConnection = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Covid19Theme)
+        isConnection = NetworkUtil.isConnection(this)
+        if (!isConnection) {
+            showDialogCheckInternet().show()
+            return
+        }
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentMainNavHost) as NavHostFragment
         val navController = navHostFragment.navController
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun showDialogCheckInternet(): AlertDialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.title_internet))
+            .setMessage(getString(R.string.text_warning))
+            .setPositiveButton(getString(R.string.titke_ok)) { _, _ ->
+                startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
+                finish()
+            }
+            .setNegativeButton(getString(R.string.title_cancel)) { _, _ ->
+                finish()
+            }
+        return builder.create()
     }
 }
